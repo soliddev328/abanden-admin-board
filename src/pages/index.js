9334e97d64291e2321c3e72db47d9b835d15fa78
Robert Zhang <solid.dev3028@gmail.com>
2019-07-31 21:40:41 -0500

    Adding Helmet for metadata

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { Helmet } from 'react-helmet'
 import '../styles/index.css'
 import landingStyles from '../styles/landing.module.css'
 import portrait from '../images/me.png'
@@ -7,6 +8,10 @@ import portrait from '../images/me.png'
 const IndexPage = () => {
   return (
     <div>
+      <Helmet>
+          <meta charSet="utf-8" />
+          <title>Morgan Richardson's Portfolio</title>
+      </Helmet>
       <Layout>
         <h1 className={landingStyles.hello}>HI, I'M MORGAN RICHARDSON.</h1>
         <h2 className={landingStyles.info}>I'M CURRENTLY STUDYING SOFTWARE ENGINEERING IN CHICAGO. WHEN I'M NOT EXPLORING THE CITY, I'M BUILDING, CREATING, AND LEARNING.</h2>
