c693a08a7602d49f5c23e74b429fb7fa99142d73
Robert Zhang <solid.dev3028@gmail.com>
2019-08-04 00:17:16 -0500

    Final contact page updates

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -12,10 +12,10 @@ const Contact = () => {
             <img src= { reachOut } className="arms" alt="pic"></img>
           </div>
           <div className="right">
-            <p className="contact-link"><a href="mailto:morganrmarie@gmail.com?subject=morgan.codes inquiry">email</a></p>
-            <p className="contact-link"><a href="https://www.github.com/morganrmarie" target="_blank" rel="noopener noreferrer">github</a></p>
-            <p className="contact-link"><a href="https://www.linkedin.com/in/morganrmarie" target="_blank" rel="noopener noreferrer">linkedin</a></p>
-            <p className="contact-link"><a href="https://www.instagram.com/morgan.codes" target="_blank" rel="noopener noreferrer">instagram</a></p>
+            <p className="contact-link"><a className="a-contact-link" href="mailto:morganrmarie@gmail.com?subject=morgan.codes inquiry">email</a></p>
+            <p className="contact-link"><a className="a-contact-link" href="https://www.github.com/morganrmarie" target="_blank" rel="noopener noreferrer">github</a></p>
+            <p className="contact-link"><a className="a-contact-link" href="https://www.linkedin.com/in/morganrmarie" target="_blank" rel="noopener noreferrer">linkedin</a></p>
+            <p className="contact-link"><a className="a-contact-link" href="https://www.instagram.com/morgan.codes" target="_blank" rel="noopener noreferrer">instagram</a></p>
           </div>
         </div>
       </Layout>
