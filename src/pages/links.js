7a8110d5576b5b7b63561ab37a41c7dd701caf38
Robert Zhang <solid.dev3028@gmail.com>
2019-09-30 21:23:29 -0500

    Removing link

--- src/pages/links.js
+++ src/pages/links.js
@@ -8,7 +8,6 @@ const Links = () => {
     <div>
       <Layout>
         <div className="link-container">
-          <div className="link-box"><a className="a-contact-link" href="https://www.attwizardsunite.com/chi" target="_blank" rel="noopener noreferrer">AT&T</a></div>
           <div className="link-box"><a className="a-contact-link" href="https://www.github.com/morganrmarie" target="_blank" rel="noopener noreferrer">GITHUB</a></div>
           <div className="link-box"><Link className="a-contact-link" to="/blogposts">BLOG</Link></div>
           <div className="link-box"><a className="a-contact-link" href="mailto:morganrmarie@gmail.com?subject=morgan.codes inquiry">EMAIL</a></div>
