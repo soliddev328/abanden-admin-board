7b4c21e96c15cbf16231da8d18de3599ba58e4e5
Robert Zhang <solid.dev3028@gmail.com>
2019-11-07 00:40:43 -0600

    Adding component for individual listings

---
+++ src/components/singleItem.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Helmet from 'react-helmet'
+import Layout from '../components/layout'
+import '../styles/single-item.css'
+
+const SingleItem = (props) => {
+    return (
+        <div className="item-container">
+
+        </div>
+    );
+}
+  
+  export default SingleItem
