1920040c55190ebc8762eb69e1dcc1988173e6b7
Robert Zhang <solid.dev3028@gmail.com>
2019-11-08 01:36:54 -0600

    Finalizing single item component

--- src/components/singleItem.js
+++ src/components/singleItem.js
@@ -1,17 +1,13 @@
 import React from 'react'
-import { Link } from 'gatsby'
-
-import Helmet from 'react-helmet'
-import Layout from '../components/layout'
 import '../styles/single-item.css'
 
 const SingleItem = (props) => {
     return (
         <div className="item-container">
-            <a href={ props.imageLink }>
-                <img src={ props.imageSource } className="item-image"></img>
+            <a target="_blank" rel="noopener noreferrer" href={ props.imageLink }>
+                <img src={ props.imageSource } className="item-image" alt="product"></img>
             </a>
-            <a href={ props.imageLink }>
+            <a target="_blank" rel="noopener noreferrer" href={ props.imageLink }>
                 <p class="item-title">{ props.title }</p>
             </a>
             <p className="item-price">{props.price}</p>
