44da8a19e6d5e0ab0fbecf4fb8797f80c95fa843
Robert Zhang <solid.dev3028@gmail.com>
2020-05-10 21:33:36 -0500

    Update Query

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,42 +2,45 @@ const path = require('path');
 const slash = require(`slash`);
 
 exports.createPages = ({ graphql, actions }) => {
-  const { createPage } = actions;
+	const { createPage } = actions;
 
-  const blogPost = path.resolve('./src/templates/blogpost.js')
-  return graphql(
-    `
-      {
-        allContentfulSingleBlogPost {
-          edges {
-            node {
-              slug
-              welcomeToMyBlog
-            }
-          }
-        }
-      }
-    `
-  ).then(result => {
-    if (result.errors) {
-      console.log('Error retrieving contenful data: ', result.errors);
-    }
+	const blogPost = path.resolve('./src/templates/blogpost.js');
+	return graphql(
+		`
+			{
+				allContentfulBlogPost {
+					edges {
+						node {
+							slug
+							welcomeToMyBlog
+						}
+					}
+				}
+			}
+		`
+	)
+		.then((result) => {
+			if (result.errors) {
+				console.log('Error retrieving contenful data: ', result.errors);
+			}
 
-    // Create blog posts createPages
-    const blogPostTemplate = path.resolve("./src/templates/blogpost.js");
+			// Create blog posts createPages
+			const blogPostTemplate = path.resolve(
+				'./src/templates/blogpost.js'
+			);
 
-    result.data.allContentfulBlogPost.edges.forEach(edge => {
-        createPage({
-          path: `/blogpost/${edge.node.slug}/`,
-          component: slash(blogPostTemplate),
-          context: {
-	           slug: edge.node.slug,
-             id: edge.node.id
-          }
-        });
-      });
-    })
-    .catch(error => {
-      console.log("Error retrieving contentful data", error);
-    });
+			result.data.allContentfulBlogPost.edges.forEach((edge) => {
+				createPage({
+					path: `/blogpost/${edge.node.slug}/`,
+					component: slash(blogPostTemplate),
+					context: {
+						slug: edge.node.slug,
+						id: edge.node.id,
+					},
+				});
+			});
+		})
+		.catch((error) => {
+			console.log('Error retrieving contentful data', error);
+		});
 };
